5981329f07a746802897c4d7b69d8a6790cfb773
Luca Arduini <arduini37@gmail.com>
2026-06-10 01:18:28 +0200

    Remove redundant DS&A submission files

--- Data Structures & Algorithms/duplicate-integer/submission-1.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-
-class Solution:
-    def hasDuplicate(self, nums: List[int]) -> bool:
-        # Crea un set dagli elementi della lista
-        num_set = set(nums)
-        # Se la lunghezza del set è inferiore a quella della lista,
-        # significa che c'erano dei duplicati che sono stati rimossi.
-        return len(num_set) != len(nums)
